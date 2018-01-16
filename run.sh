9ab4ee6ddd708fe60b8501c7dbfec7b8ea327f2e
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-01-16 21:31:47 +0100

    virtual environment

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-convert input/1.png ppm:- | python3 main.py
+if [ ! -d venv/ ]; then
+    python3 -m venv venv
+    venv/bin/pip install -r requirements.txt
+fi
+convert input/1.png ppm:- | venv/bin/python3 main.py
