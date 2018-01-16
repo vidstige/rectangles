3acc917986b0962e729a5b8e5d09529294b0b43f
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-01-16 08:08:17 +0100

    problem skeleton

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import sys
-from netpbm import imread
+from netpbm import imread, Image
+
+
+class Problem(object):
+    def __init__(self, image: Image) -> None:
+        pass
+
+    def error(self, x):
+        return 0
+
 
 def main():
-    imread(sys.stdin.buffer)
+    img = imread(sys.stdin.buffer)
+    problem = Problem(img)
+    print(problem.error(0))
+
 
 main()
