dff0cf349fe1b725f2f24d7e6b18e9a97c26c255
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-01-17 18:35:18 +0100

    test: zero size rects

--- tests/test_render.py
+++ tests/test_render.py
@@ -9,3 +9,15 @@ def test_easy():
     r = Rectangle(0, 0, 2, 2, WHITE)
     render_rectangle(r, img)
     assert_equal(img.data, b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+
+def test_zero_height():
+    img = Image(2, 2)
+    r = Rectangle(0, 0, 2, 0, WHITE)
+    render_rectangle(r, img)
+    assert_equal(img.data, b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+def test_zero_width():
+    img = Image(2, 2)
+    r = Rectangle(0, 0, 0, 2, WHITE)
+    render_rectangle(r, img)
+    assert_equal(img.data, b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
